1aec5382191fa42aef7f5041c89d6dccd8ef22bc
Monica Li <monica.amanda.li@gmail.com>
2014-08-12 20:04:39 -0400

    Old updates

--- BookstoreInventory/src/com/limonica/bookstoreinventory/MySQLiteHelper.java
+++ BookstoreInventory/src/com/limonica/bookstoreinventory/MySQLiteHelper.java
@@ -6,19 +6,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
-	
+
 	public static final String TABLE_NEWBOOKS = "newBooks";
 	public static final String COLUMN_ID = "_id";
 	public static final String COLUMN_NEWBOOKS = "newBooks";
 
 	private static final String DATABASE_NAME = "newBooks.db";
 	private static final int DATABASE_VERSION = 1;
-	
+
 	// Database creation sql statement
-	private static final String DATABASE_CREATE = "create table "
-	    + TABLE_NEWBOOKS + "(" + COLUMN_ID
-	    + " integer primary key autoincrement, " + COLUMN_NEWBOOKS
-	    + " text not null);";
+	private static final String DATABASE_CREATE = "create table " + TABLE_NEWBOOKS + "(" +
+		COLUMN_ID + " integer primary key autoincrement, " +
+		COLUMN_NEWBOOKS + " text not null)";
 
 	public MySQLiteHelper(Context context) {
 	    super(context, DATABASE_NAME, null, DATABASE_VERSION);
